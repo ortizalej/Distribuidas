fecec491d90671d6530a43ec60f6167a14dc0d6d
DESKTOP-NUCAED7\ortiz <ortizalej199@gmail.com>
2020-10-01 13:18:29 -0300

    image

--- App.js
+++ App.js
@@ -52,19 +52,9 @@ function cacheImages(images) {
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
-    loading: true
   }; 
-  async componentDidMount() {
-    await Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
-    })
-    this.setState({ loading: false })
-  }
-
   render() {
-    if (this.state.loading && !this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
         <AppLoading
           startAsync={this._loadResourcesAsync}
