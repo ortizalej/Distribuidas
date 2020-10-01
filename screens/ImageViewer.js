689ee0a03d6861f92bc2ff52076a3c802479e61a
DESKTOP-NUCAED7\ortiz <ortizalej199@gmail.com>
2020-10-01 12:35:21 -0300

    image

---
+++ screens/ImageViewer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {
+    StyleSheet,
+    Dimensions,
+    ScrollView,
+    AsyncStorage,
+    ImageBackground,
+    View,
+    Image,
+
+} from 'react-native'
+import { Button, Block } from 'galio-framework'
+import { showMessage, hideMessage } from 'react-native-flash-message'
+import Form from '../components/Formulario'
+const { width, height } = Dimensions.get('screen')
+
+export default class ImageViewer extends React.Component {
+    render() {
+        console.log('PROPS', this.props)
+        return (
+            <View>
+                <Image source={{uri: this.props.data}} style={
+                    { width: 200, height: 200, alignContent:"center",marginLeft: 60, marginTop: -100, marginBottom: 200}} />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    login: {
+        width: width,
+        height: height
+    },
+    backgoundContainer: {
+        flex: 1,
+        width: 500,
+        height: null,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
